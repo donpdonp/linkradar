4ffbe823d857f7bb18bc72b46e6a8335a93303a7
Don Park <donp@donp.org>
2023-04-26 13:55:53 -0700

    split files

---
+++ src/app.rs
@@ -0,0 +1,36 @@
+use eframe::egui;
+use eframe::epaint;
+
+pub struct MyApp {
+    times: u32,
+}
+
+impl MyApp {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        MyApp { times: 4 }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let win_size = frame.info().window_info.size;
+            ui.heading("linkmonitor");
+            let circle = epaint::CircleShape {
+                center: egui::Pos2 {
+                    x: win_size.x/2.0,
+                    y: self.times as f32,
+                },
+                radius: win_size.x*0.2,
+                fill: egui::Color32::RED,
+                stroke: egui::Stroke::new(4.0, egui::Color32::YELLOW),
+            };
+            ui.painter().add(egui::Shape::Circle(circle));
+            self.times += 1;
+        });
+    }
+}
