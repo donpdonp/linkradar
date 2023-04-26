535bbe2a03a452fcbee94ee057320e0c28296b4e
Don Park <donp@donp.org>
2023-04-26 13:44:24 -0700

    center the dot

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use eframe::egui;
-use eframe::egui::ImageData::Color;
-use eframe::egui::Shape::Circle;
 use eframe::egui::{Color32, Pos2, Shape, Stroke};
 use eframe::epaint::CircleShape;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        initial_window_size: Some(egui::vec2(40.0, 440.0)),
         ..Default::default()
     };
     eframe::run_native(
-        "My egui App",
+        "LM",
         options,
         Box::new(|cc| Box::new(MyApp::new(cc))),
     )
@@ -21,7 +19,7 @@ struct MyApp {
 }
 
 impl MyApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
@@ -33,15 +31,16 @@ impl MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
+            let win_size = frame.info().window_info.size;
+            ui.heading("linkmonitor");
             let circle = CircleShape {
                 center: Pos2 {
-                    x: self.times as f32,
-                    y: 0.0,
+                    x: win_size.x/2.0,
+                    y: self.times as f32,
                 },
-                radius: 40.0,
+                radius: win_size.x*0.2,
                 fill: Color32::RED,
-                stroke: Stroke::new(14.0, Color32::YELLOW),
+                stroke: Stroke::new(4.0, Color32::YELLOW),
             };
             ui.painter().add(Shape::Circle(circle));
             self.times += 1;
