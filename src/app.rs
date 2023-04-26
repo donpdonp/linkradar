99af065c0f18c364f7565c2516b7f8b768100dfa
Don Park <donp@donp.org>
2023-04-26 14:54:26 -0700

    ping and log

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,10 @@
+use eframe::egui;
+use eframe::epaint;
+use fastping_rs::{PingResult, Pinger};
+use log;
 use std::thread;
 use std::thread::spawn;
 use std::time::Duration;
-use eframe::egui;
-use eframe::epaint;
 
 pub struct MyApp {
     times: u32,
@@ -10,18 +12,36 @@ pub struct MyApp {
 
 impl MyApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let (pinger, results) = match Pinger::new(None, Some(56)) {
+            Ok((pinger, results)) => (pinger, results),
+            Err(e) => panic!("Error creating pinger: {}", e),
+        };
+        pinger.add_ipaddr("4.2.2.2");
+        pinger.run_pinger();
+
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         let ectx = cc.egui_ctx.clone();
 
-        spawn (move || {
-            loop {
-                ectx.request_repaint();
-                thread::sleep(Duration::from_secs(1));
+        spawn(move || loop {
+            match results.recv() {
+                Ok(result) => match result {
+                    PingResult::Idle { addr } => {
+                        log::error!("Idle Address {}.", addr);
+                    }
+                    PingResult::Receive { addr, rtt } => {
+                        log::info!("Receive from Address {} in {:?}.", addr, rtt);
+                    }
+                },
+                Err(_) => panic!("Worker threads disconnected before the solution was found!"),
             }
+
+            ectx.request_repaint();
+            thread::sleep(Duration::from_secs(1));
         });
+
         MyApp { times: 4 }
     }
 }
@@ -33,10 +53,10 @@ impl eframe::App for MyApp {
             ui.heading("linkmonitor");
             let circle = epaint::CircleShape {
                 center: egui::Pos2 {
-                    x: win_size.x/2.0,
+                    x: win_size.x / 2.0,
                     y: self.times as f32,
                 },
-                radius: win_size.x*0.2,
+                radius: win_size.x * 0.2,
                 fill: egui::Color32::RED,
                 stroke: egui::Stroke::new(4.0, egui::Color32::YELLOW),
             };
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@ mod app;
 
 use eframe::egui;
 
-
 fn main() -> Result<(), eframe::Error> {
+    env_logger::init();
+    log::info!("linkradar");
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(40.0, 440.0)),
         ..Default::default()
     };
-    eframe::run_native(
-        "LM",
-        options,
-        Box::new(|cc| Box::new(app::MyApp::new(cc))),
-    )
-}
 
+    eframe::run_native("LM", options, Box::new(|cc| Box::new(app::MyApp::new(cc))))
+}
